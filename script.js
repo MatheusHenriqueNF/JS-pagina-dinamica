a6510a14b9c9fd102c57c9ccf9f97c49abdd870d
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2024-11-28 18:34:57 -0300

    organizando código

---
+++ script.js
@@ -0,0 +1,8 @@
+const item = document.getElementById('input-item');
+const botaoSalvarItem = document.getElementById('adicionar-botao');
+
+botaoSalvarItem.addEventListener('click', adicionarItem);
+
+function adicionarItem() {
+    console.log('adicionar item');
+}
